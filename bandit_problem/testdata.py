3b1167c8cfa7eb409d631e978d882586a5bfb35f
sallllly0307 <sara.love.choco.0307@gmail.com>
2020-12-13 18:18:16 +0900

    Implement exponential random number generator

--- bandit_problem/testdata.py
+++ bandit_problem/testdata.py
@@ -1,4 +1,7 @@
 import numpy as np
 import numpy.random as rd
-import scipy.stats as st
 import matplotlib.pyplot as plt
+
+lam = 0.1   # 1分あたり0.1回発生する。
+x = rd.exponential(1./lam, size=20)
+print(x)
