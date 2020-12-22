5441f7ec43d94fc91e8acc2714e61d94d70c6778
sallllly0307 <sara.love.choco.0307@gmail.com>
2020-12-22 19:35:45 +0900

    fix import test data

--- bandit_problem/testdata.py
+++ bandit_problem/testdata.py
@@ -4,16 +4,4 @@ import matplotlib.pyplot as plt
 import datetime
 
 lam = 0.1
-x = rd.exponential(0.01/lam, size=100)
-
-y = []
-for elem in x:
-    s = str(elem)
-    y.append(s)
-print(", ".join(y))
-plt.hist(x, bins=15)
-
-
-now = datetime.datetime.now()
-filename = 'testdata' + now.strftime('%Y%m%d_%H%M%S')
-plt.savefig('testdata' + str(now) + '.png')
+exponential = rd.exponential(0.1/lam, size=100)
